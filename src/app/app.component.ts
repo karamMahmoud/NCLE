3b7906edef3451e6c4d70836f7cde2815c448096
Karam Mahmoud <k.mahmoud@elham.sa>
2025-04-13 23:46:06 +0200

    feat: Add the shared header

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: false,
+})
+export class AppComponent {
+  title = 'Surprise Box - Educational Subscription Box for Kids';
+
+  // scrollToSection(sectionId: string): void {
+  //   const element = document.getElementById(sectionId);
+  //   if (element) {
+  //     element.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // }
+}
